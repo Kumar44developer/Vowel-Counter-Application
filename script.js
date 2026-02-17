bc922fc70a184376244c289809f357876dd97944
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-02-17 18:12:20 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 function checkVowels(){
 
-
  let text = document.getElementById("inputText").value;
  let vowelCount = 0;
 
   text = text.toLowerCase();
 
   for(let i = 0; i < text.length; i++){
+
     let char = text.charAt(i);
     if (isVowel(char)){
         vowelCount++;
     }
   }
 
- const result = document.getElementById('result');
- result.textContent = "Total Vowels: " + vowelCount;
+    
+  const result = document.getElementById('result');
+  result.textContent = "Total Vowels: " + vowelCount;
 
 
 }
